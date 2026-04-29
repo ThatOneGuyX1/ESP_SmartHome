dc39c00423e7e1331709bc4d259b8dbc2d7651be
ThatOneGuyX1 <iansearle0@gmail.com>
2026-04-28 20:17:27 -0600

    Bug Fixes from Merge

--- Nodes/host/Host_node.py
+++ Nodes/host/Host_node.py
@@ -5,9 +5,31 @@ sh.boot()
 
 LIGHT_1_MAC = sh.mac_bytes(sh.PEER_DICT["light_1"]["mac"])
 
-while True:
-    test_pkt = bytes([sh.ACT_TEST]) + bytes(66)  # 67 bytes total
+
+test_pkt = sh.create_msg_packet(
+    dest_mac = LIGHT_1_MAC,
+    action   = sh.ACT_TEST,
+    message  = b''
+)
+sh.espnow_send(LIGHT_1_MAC, test_pkt)
+print("[TEST] Ping sent to light_1")
+time.sleep(2)
+sh.poll_serial()
+
+cycle = 0
+
+while cycle < 5:
+
+    test_pkt = sh.create_msg_packet(
+    dest_mac = LIGHT_1_MAC,
+    action   = sh.ACT_REQ_ACTION,
+    message  = b''
+)
     sh.espnow_send(LIGHT_1_MAC, test_pkt)
-    print("[TEST] Ping sent to light_1")
+    print("[Action Request] Ping sent to light_1")
     time.sleep(2)
     sh.poll_serial()
+    cycle += 1
+
+
+
